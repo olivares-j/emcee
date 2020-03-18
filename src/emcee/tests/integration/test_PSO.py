aa89d99b2daccf01aaaadbe3c57c0f672828e243
olivares-j <avijer@gmail.com>
2020-03-18 18:44:44 +0100

    Includes PSO normal test

---
+++ src/emcee/tests/integration/test_PSO.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+import numpy as np
+import emcee
+
+from emcee import moves
+
+__all__ = [
+    "test_normal_pso",
+    "test_uniform_pso"
+]
+
+def normal_log_prob_blobs(params):
+    return -0.5 * np.sum(params ** 2), params
+
+
+def normal_log_prob(params):
+    return -0.5 * np.sum(params ** 2)
+
+def _test_normal(
+    proposal,
+    ndim=1,
+    nwalkers=32,
+    nsteps=2000,
+    seed=1234,
+    check_acceptance=True,
+    pool=None,
+    blobs=False,
+):
+    # Set up the random number generator.
+    np.random.seed(seed)
+
+    # Initialize the ensemble and proposal.
+    coords = np.random.randn(nwalkers, ndim)
+
+    if blobs:
+        lp = normal_log_prob_blobs
+    else:
+        lp = normal_log_prob
+
+    sampler = emcee.EnsembleSampler(
+        nwalkers, ndim, lp, moves=proposal, pool=pool
+    )
+    if hasattr(proposal, "ntune") and proposal.ntune > 0:
+        coords = sampler.run_mcmc(coords, proposal.ntune, tune=True)
+        sampler.reset()
+    sampler.run_mcmc(coords, nsteps)
+
+    # Check the acceptance fraction.
+    if check_acceptance:
+        acc = sampler.acceptance_fraction
+        assert np.all(
+            (acc <= 1.0) * (acc >= 0.0)
+        ), "Invalid acceptance fraction\n{0}".format(acc)
+
+    # Check that the MAP is within tolerance
+    logps = sampler.get_log_prob(flat=True)
+    samps = sampler.get_chain(flat=True)
+
+    idx_map = logps.argmax()
+    mu_map  = samps[idx_map]
+    assert np.all(np.abs(mu_map) < 0.08), "Incorrect mean"
+
+
+@pytest.mark.parametrize("blobs", [True,False])
+def test_normal_pso(blobs, **kwargs):
+    kwargs["blobs"] = blobs
+    _test_normal(moves.PSOMove(), **kwargs)
+
